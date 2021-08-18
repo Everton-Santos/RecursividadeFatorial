2575a6a9d9f9e2d20b5de203dc769149e195e477
Everton Santos <everton_santos_01@hotmail.com>
2021-08-18 13:49:52 -0300

    Primeiro commit

---
+++ src/view/Principal.java
@@ -0,0 +1,18 @@
+package view;
+
+import controller.FatorialController;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		//Método construtor
+		FatorialController ft = new FatorialController();
+		//Definindo um valor para o fatorial
+		int fatorial = 4;
+		//variavel que recebe o resultado
+		int resultado = ft.fat(fatorial);
+		System.out.println("Resultado: "+resultado);
+
+	}
+
+}
